9e8360fc64167a960694ab045709d497962626d6
kallecheMedy <144663353+kallecheMedy@users.noreply.github.com>
2024-10-15 19:52:49 +0000

    slt

--- login.php
+++ login.php
@@ -45,7 +45,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/resources/labheader/css/academyLabHeader.css" rel="stylesheet">
+    <link href="academyLabHeader.css" rel="stylesheet">
     <title>Connexion</title>
 </head>
 <body>
